e14009b036124dbabfe52b243900919d87b132ee
sylvain <sylvain@kinetix.tech>
2024-01-30 11:50:18 +0100

    [FIX] bump manifest action

--- .github/actions/bump-manifest-version.js
+++ .github/actions/bump-manifest-version.js
@@ -3,8 +3,13 @@ const fs = require('fs')
 const version = process.env.TGT_RELEASE_VERSION
 const newVersion = version.replace('v', '')
 
-const manifestFile = fs.readFileSync('fxmanifest.lua', {encoding: 'utf8'})
+// Update kinetix_mod manifest
+const modManifestFile = fs.readFileSync('kinetix_mod/fxmanifest.lua', {encoding: 'utf8'})
+const newModFileContent = modManifestFile.replace(/\bversion\s+(.*)$/gm, `version      '${newVersion}'`)
+fs.writeFileSync('kinetix_mod/fxmanifest.lua', newModFileContent)
 
-const newFileContent = manifestFile.replace(/\bversion\s+(.*)$/gm, `version      '${newVersion}'`)
 
-fs.writeFileSync('fxmanifest.lua', newFileContent)
+// Update kinetix_anim manifest
+const animManifestFile = fs.readFileSync('kinetix_anim/fxmanifest.lua', {encoding: 'utf8'})
+const newAnimFileContent = animManifestFile.replace(/\bversion\s+(.*)$/gm, `version      '${newVersion}'`)
+fs.writeFileSync('kinetix_anim/fxmanifest.lua', newAnimFileContent)
